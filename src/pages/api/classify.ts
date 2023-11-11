8c1632451ed6c12a8409f0fb7b5b6a0d4e1da2f7
Ryan Huellen <ryan@huellenconsulting.com>
2023-11-11 16:09:56 -0600

    feat: classification api route

--- src/pages/api/classify.ts
+++ src/pages/api/classify.ts
@@ -1,77 +1,93 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import NextConnect from "next-connect";
+import multer from "multer";
 
-type Data = {
-  text: string;
-};
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Response = {
   message: string;
-  data?: Data;
-};
-
-export const handler = async (
-  req: NextApiRequest,
-  res: NextApiResponse<Response>
-) => {
-  if (req.method !== "POST") {
-    res.status(405).json({ message: "Method not allowed" });
-  }
-
-  if (!req.body) {
-    res.status(400).json({ message: "No image provided" });
-  }
-
-  const base64ImageUrl = `data:image/jpeg;base64,${req.body}`;
-
-  const response = await fetch("https://api.openai.com/v1/chat/completions", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization:
-        "Bearer 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",
-    },
-    body: JSON.stringify({
-      model: "gpt-4-vision-preview",
-      messages: [
-        {
-          role: "user",
-          content: [
-            {
-              type: "text",
-              text: "Classify the given space imagery. If the image is not from space, please say so.",
-            },
-            {
-              type: "image_url",
-              image_url: {
-                url: base64ImageUrl,
-              },
-            },
-          ],
-        },
-      ],
-      max_tokens: 300,
-    }),
-  });
-
-  const json = (await response.json()) as {
-    choices: {
-      message: {
-        content: string;
-      };
+  data?: {
+    classifications: {
+      filename: string;
+      classification: string;
     }[];
   };
+};
 
-  if (json.choices.length === 0) {
-    res
-      .status(500)
-      .json({ message: "Couldn't classify image. Please try again." });
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const multerInstance = multer({
+  storage: multer.memoryStorage(),
+});
+
+const handler = NextConnect<NextApiRequest, NextApiResponse>({
+  onNoMatch(_, res) {
+    res.status(405).json({ message: "Method not allowed" } satisfies Response);
+  },
+});
+
+handler.post(multerInstance.array("images"), async (req, res) => {
+  const filesArray = req.files as Express.Multer.File[];
+
+  if (!filesArray) {
+    res.status(400).json({ message: "No files provided" } satisfies Response);
+    return;
   }
+
+  const classifications = await Promise.all(
+    filesArray.map(async (file) => {
+      const response = await fetch(
+        "https://api.openai.com/v1/chat/completions",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+          },
+          body: JSON.stringify({
+            model: "gpt-4-vision-preview",
+            messages: [
+              {
+                role: "user",
+                content: [
+                  {
+                    type: "text",
+                    text: "Classify the given space imagery. If the image is not from space, please say so.",
+                  },
+                  {
+                    type: "image_url",
+                    image_url: {
+                      url: `data:${file.mimetype};base64,${file.buffer.toString(
+                        "base64"
+                      )}`,
+                    },
+                  },
+                ],
+              },
+            ],
+            max_tokens: 300,
+          }),
+        }
+      );
+
+      const json = await response.json();
+
+      return {
+        filename: file.filename,
+        classification: json.choices[0].message.content,
+      };
+    })
+  );
 
   res.status(200).json({
     message: "Success",
     data: {
-      text: json.choices[0].message.content,
+      classifications,
     },
-  });
-};
+  } satisfies Response);
+});
+
+export default handler;
