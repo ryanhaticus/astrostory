3545528ce89d996257e2fde2317afdb99761ba28
Ryan Huellen <ryan@huellenconsulting.com>
2023-11-11 19:58:40 -0600

    fix: added index key

--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -15,8 +15,11 @@ const Story: React.FC<StoryInputProps> = ({ story }) => {
   return (
     <div>
       <div className="flex flex-col gap-4">
-        {story.map((story) => (
-          <div className="rounded-xl w-full flex gap-4 rounded-md bg-gray-800 p-4 text-sm font-semibold text-white shadow-sm">
+        {story.map((story, index) => (
+          <div
+            key={index}
+            className="rounded-xl w-full flex gap-4 rounded-md bg-gray-800 p-4 text-sm font-semibold text-white shadow-sm"
+          >
             <img className="rounded-xl w-64" src={story.image} />
             <p className="text-lg">{story.text}</p>
           </div>
