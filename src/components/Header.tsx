54b569cafddda91a5870cc138c0cd82a2b8a619f
Ryan Huellen <ryan@huellenconsulting.com>
2023-11-11 18:49:32 -0600

    feat: improved header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,3 @@
-// components/Header.tsx
-//import { ChevronRightIcon } from '@heroicons/react/20/solid';
-
 interface HeaderProps {
   title: string;
   subtitle: string;
@@ -8,44 +5,14 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ title, subtitle }) => {
   return (
-    <div className="relative bg-gray-900">
-      {/* ... SVG background and decorative elements ... */}
-      <div className="relative z-10 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center py-6 md:justify-start md:space-x-10">
-          <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="#">
-              <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto sm:h-10"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                alt=""
-              />
-            </a>
-          </div>
-          <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-            <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
-              Sign in
-            </a>
-            <a
-              href="#"
-              className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-            >
-              Sign up
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="mx-auto max-w-7xl px-4 sm:px-6">
-        <div className="text-center">
-          <h1 className="text-4xl tracking-tight font-extrabold text-white sm:text-5xl md:text-6xl">
-            <span className="block xl:inline">{title}</span>
-          </h1>
-          <p className="mt-3 max-w-md mx-auto text-base text-gray-300 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-            {subtitle}
-          </p>
-        </div>
-      </div>
-    </div>
+    <>
+      <h1 className="text-4xl tracking-tight font-extrabold text-white sm:text-5xl md:text-6xl">
+        {title}
+      </h1>
+      <p className="mt-3 text-base text-gray-300 sm:text-lg md:mt-5 md:text-xl">
+        {subtitle}
+      </p>
+    </>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,88 @@
-import React, { useState } from 'react';
-import Header from '../components/Header';
-import StoryInput from '../components/StoryInput';
-import StoryDisplay from '../components/StoryDisplay';
-import ToneSelection from '../components/ToneSelection'; // Import the ToneSelection component
+import React, { useState } from "react";
+import Header from "../components/Header";
+import StoryInput from "../components/StoryInput";
+import StoryDisplay from "../components/StoryDisplay";
+import ToneSelection from "../components/ToneSelection"; // Import the ToneSelection component
 
 const Home = () => {
-  const [textPrompt, setTextPrompt] = useState('');
+  const [textPrompt, setTextPrompt] = useState("");
   const [uploadedImages, setUploadedImages] = useState<FileList | null>(null);
-  const [selectedTone, setSelectedTone] = useState('');
+  const [selectedTone, setSelectedTone] = useState("");
 
   const handleTextSubmit = (text: string) => {
-    console.log('Text submitted:', text);
+    console.log("Text submitted:", text);
     setTextPrompt(text);
   };
 
   const handleImagesSubmit = (images: FileList | null) => {
-    console.log('Images submitted:', images);
+    console.log("Images submitted:", images);
     setUploadedImages(images);
   };
 
   const handleToneSelect = (tone: string) => {
-    console.log('Tone selected:', tone);
+    console.log("Tone selected:", tone);
     setSelectedTone(tone);
   };
 
   return (
-    <>
-      <Header 
-        title="AstroClassify" 
-        subtitle="Embark on a journey through the cosmos and create your own space tales."
-      />
+    <div className="min-h-screen relative isolate overflow-hidden bg-gray-900">
+      <svg
+        className="absolute inset-0 -z-10 h-full w-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+        aria-hidden="true"
+      >
+        <defs>
+          <pattern
+            id="983e3e4c-de6d-4c3f-8d64-b9761d1534cc"
+            width={200}
+            height={200}
+            x="50%"
+            y={-1}
+            patternUnits="userSpaceOnUse"
+          >
+            <path d="M.5 200V.5H200" fill="none" />
+          </pattern>
+        </defs>
+        <svg x="50%" y={-1} className="overflow-visible fill-gray-800/20">
+          <path
+            d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
+            strokeWidth={0}
+          />
+        </svg>
+        <rect
+          width="100%"
+          height="100%"
+          strokeWidth={0}
+          fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)"
+        />
+      </svg>
+      <div
+        className="absolute left-[calc(50%-4rem)] top-10 -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]"
+        aria-hidden="true"
+      >
+        <div
+          className="aspect-[1108/632] w-[69.25rem] bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-20"
+          style={{
+            clipPath:
+              "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)",
+          }}
+        />
+      </div>
+      <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex flex-col lg:px-8 lg:py-40">
+        <Header
+          title="AstroClassify"
+          subtitle="Embark on a journey through the cosmos and create your own space tales."
+        />
 
-      {/* Integrate the ToneSelection component */}
-      <ToneSelection onToneSelect={handleToneSelect} />
+        <ToneSelection onToneSelect={handleToneSelect} />
 
-      <StoryInput 
-        onTextSubmit={handleTextSubmit} 
-        onImagesSubmit={handleImagesSubmit} 
-      />
-      <StoryDisplay 
-        textPrompt={textPrompt} 
-        uploadedImages={uploadedImages} 
-        selectedTone={selectedTone} // Pass the selected tone to StoryDisplay
-      />
-    </>
+        <StoryInput
+          onTextSubmit={handleTextSubmit}
+          onImagesSubmit={handleImagesSubmit}
+        />
+        <StoryDisplay textPrompt={textPrompt} uploadedImages={uploadedImages} />
+      </div>
+      <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32"></div>
+    </div>
   );
 };
 
